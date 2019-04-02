9fdcb5a535ad1344294cab8b139ea0c680803074
TheAssassin <theassassin@assassinate-you.net>
2019-04-02 22:53:38 +0200

    Implement basic build command
    Just generates scripts and calls main build script. Works fine for now.

--- appimagecraft/commands/__init__.py
+++ appimagecraft/commands/__init__.py
@@ -1,5 +1,6 @@
 from .base import CommandBase
+from .build import BuildCommand
 from .genscripts import GenerateScriptsCommand
 
 
-__all__ = ("CommandBase", "GenerateScriptsCommand",)
+__all__ = ("CommandBase", "GenerateScriptsCommand", "BuildCommand")
---
+++ appimagecraft/commands/build.py
@@ -0,0 +1,34 @@
+import subprocess
+
+from . import CommandBase
+from ..generators import AllBuildScriptsGenerator
+from .. import _logging
+
+
+class BuildCommand(CommandBase):
+    def __init__(self, config: dict, project_root_dir: str, build_dir: str, builder_name: str):
+        super().__init__(config, project_root_dir, build_dir, builder_name)
+
+        self._logger = _logging.get_logger("build")
+
+    def set_build_dir(self, build_dir: str):
+        self._build_dir = build_dir
+
+    def _get_gen(self) -> AllBuildScriptsGenerator:
+        gen = AllBuildScriptsGenerator(self._config, self._project_root_dir, self._builder_name)
+
+        return gen
+
+    def run(self):
+        self._logger.info("Generating build scripts in {}".format(self._build_dir))
+
+        if self._build_dir is None:
+            raise ValueError("build dir has not been set")
+
+        gen = self._get_gen()
+
+        build_script = gen.generate_all_scripts(self._build_dir)
+
+        self._logger.info("Calling main build script {}".format(build_script))
+
+        subprocess.check_call([build_script])
