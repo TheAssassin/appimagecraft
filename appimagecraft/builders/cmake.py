f8a9c2cbd3068f81ab5033c2a6c74029c2b121b4
TheAssassin <theassassin@assassinate-you.net>
2019-11-22 16:33:40 +0100

    Add support for different source dir projects

--- appimagecraft/builders/cmake.py
+++ appimagecraft/builders/cmake.py
@@ -47,6 +47,17 @@ class CMakeBuilder(BuilderBase):
         if " " in name:
             raise ValueError("Spaces are not allowed in CMake argument names")
 
+    def _get_source_dir(self, project_root_dir):
+        source_dir = self._cmake_conf.get("source_dir", None)
+
+        if not source_dir:
+            return project_root_dir
+
+        if not os.path.isabs(source_dir):
+            source_dir = os.path.join(project_root_dir, source_dir)
+
+        return source_dir
+
     def _generate_cmake_command(self, project_root_dir: str):
         args = ["cmake"]
 
@@ -57,7 +68,8 @@ class CMakeBuilder(BuilderBase):
 
             args.append("-D{}={}".format(key, escaped_value))
 
-        args.append(project_root_dir)
+        source_dir = self._get_source_dir(project_root_dir)
+        args.append(source_dir)
 
         return " ".join(args)
 
