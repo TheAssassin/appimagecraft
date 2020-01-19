b7829684f1ae24998b93ade5f65f2b6971357296
TheAssassin <theassassin@assassinate-you.net>
2020-01-20 00:00:32 +0100

    Add example QMake project

---
+++ example-projects/qmake/example.pro
@@ -0,0 +1,9 @@
+TEMPLATE = app
+
+SOURCES += main.cpp
+CONFIG -= qt
+
+target.path = /usr/bin
+target.TARGET = example
+
+INSTALLS += target
---
+++ example-projects/qmake/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    (void) argc;
+    (void) argv;
+
+    std::cout << "Hello world, this is the appimagecraft qmake example project." << std::endl;
+    return 0;
+}
