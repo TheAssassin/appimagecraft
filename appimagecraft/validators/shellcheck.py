628a9c63bea19ca02af1e3d913c4f63868f8dc22
TheAssassin <theassassin@assassinate-you.net>
2022-09-05 22:14:58 +0200

    Bundle static shellcheck binary
    shellcheck apparently dropped support for the (very much legacy) i386 world already, hence we now only bundle shellcheck for x86_64. In the long term, this repository is also going to drop support for i386. It hasn't made _any_ sense to run i386 operating systems anymore in the last decade anyway.
    
    This commit also extracts the desktop file, icon and AppRun script into separate files in the repository.
    
    Note for self: Python's shutil is great.

--- appimagecraft/validators/shellcheck.py
+++ appimagecraft/validators/shellcheck.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from . import ValidatorBase
 from .exceptions import ValidationError
@@ -11,23 +12,31 @@ class ShellCheckValidator(ValidatorBase):
         super().__init__()
 
     @staticmethod
+    def _find_shellcheck():
+        shellcheck_env = os.environ.get("SHELLCHECK", None)
+
+        if shellcheck_env:
+            return shellcheck_env
+
+        return shutil.which("shellcheck")
+
+    @staticmethod
     def is_available() -> bool:
-        try:
-            with open(os.devnull, "w") as devnull:
-                check_call(["command", "-v", "shellcheck"], stderr=devnull, stdout=devnull, shell=True)
-        except CalledProcessError:
-            return False
-        else:
-            return True
+        return bool(ShellCheckValidator._find_shellcheck())
 
     @staticmethod
     def supported_file_types():
         return ["*.sh", "*.bash"]
 
     def validate(self, path: str):
+        shellcheck_path = self._find_shellcheck()
+
+        if not shellcheck_path:
+            raise ValidationError("could not find shellcheck")
+
         try:
             # SC2116 can be ignored safely, it's just about a "useless echo", but we want to test the version_cmd
             # feature with an echo call
-            check_call(["shellcheck", "-e", "SC2116", "-e", "SC1091", "-x", path], cwd=os.path.dirname(path))
+            check_call([shellcheck_path, "-e", "SC2116", "-e", "SC1091", "-x", path], cwd=os.path.dirname(path))
         except CalledProcessError:
             raise ValidationError("failed to run shellcheck")
---
+++ deployment/AppRun.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+this_dir="$(dirname "$0")"
+
+# we only bundle shellcheck for platforms for which shellcheck officially provides static binaries
+# our launch script therefore needs to check whether such a binary can be found in the AppDir
+# appimagecraft uses this binary if it exists, otherwise falls back to checking $PATH
+SHELLCHECK="$this_dir/usr/bin/shellcheck"
+[[ -f "$SHELLCHECK" ]] && export SHELLCHECK
+
+"$this_dir"/usr/bin/python -m appimagecraft "$@"
+EOF
